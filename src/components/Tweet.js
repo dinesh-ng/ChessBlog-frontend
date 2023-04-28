0648f82b2012ecfe9957131f7fb9159eed93f541
dinesh-ng <adultimprover@gmail.com>
2023-04-29 03:18:11 +0530

    add mui to feed

---
+++ src/components/Tweet.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+export default function RecipeReviewCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            A
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.createdBy}
+        subheader={props.createdAt}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.tweet}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton aria-label="add to favorites" onClick={props.onDelete}>
+          <DeleteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/TweetFeed.js
+++ src/components/TweetFeed.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import tweetService from "./Services";
+import Tweet from "./Tweet";
 
 const TweetFeed = () => {
   const tweetServices = new tweetService();
@@ -26,14 +27,20 @@ const TweetFeed = () => {
         {tweets &&
           tweets.map((tweet, idx) => (
             <div key={idx}>
-              <h5>
+              {/* <h5>
                 {tweet.createdBy} @ <span>{tweet.createdAt}</span>
               </h5>
               <button onClick={() => deleteTweet(tweet._id)}>
                 Delete Tweet
               </button>
 
-              <p>{tweet.tweet}</p>
+              <p>{tweet.tweet}</p> */}
+              <Tweet
+                tweet={tweet.tweet}
+                createdAt={tweet.createdAt}
+                createdBy={tweet.createdBy}
+                onDelete={() => deleteTweet(tweet._id)}
+              />
             </div>
           ))}
       </div>
